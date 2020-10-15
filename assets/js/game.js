12543e837c8eb553ca174ebfb0e0ab21385c10f3
CoCo Chanel <veta583518@gmail.com>
2020-10-14 23:33:58 -0500

    added fight function and console to JavaScript file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName 
+console.log(playerName);
+console.log("This logs a string, got for leaving yourself a message");
+//this will do math an dlog 20
+console.log(10 + 10);
+//what is this?
+console.log("Our robots's nme is " + playerName);
+
+// this creates a function named "fight"
+function fight () {
+    window.alert("The fight has begun!");
+}
+ //   fight();
