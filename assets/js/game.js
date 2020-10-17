ce476504b43202fc009c298398e6f490cb55c16e
CoCo Chanel <veta583518@gmail.com>
2020-10-16 21:10:15 -0500

    defeat all combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,14 +7,13 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-// You can also log multiple values at once like this 
-
-
-
+// Alert players that they are starting the round
+window.alert("Welcome to Robot Gladiators!");
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+  //repeat and execute as long as the enemy-robot is alive
+  while(enemyHealth > 0) {
+    //place fight function code block here
 
     // Fight Prompt
     var promptFight = window.prompt("Would you like to Fight or Skip this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -68,8 +67,11 @@ var fight = function(enemyName) {
     else {
       window.alert("You need to choose a valid option. Try again!")
     }
+  }
   };
 // execute function
 for(var i = 0; i < enemyNames.length; i++) {
-fight(enemyNames[i]);
+ var pickedEnemyName = enemyNames[i];
+ enemyHealth = 50;
+fight(pickedEnemyName);
 }
